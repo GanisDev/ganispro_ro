638c686e0ab55cf45c01a2b81d514932410370bc
GanisDev <62992703+GanisDev@users.noreply.github.com>
2023-07-13 10:31:18 +0300

    fisire pt site

---
+++ js/drexplain.data.index.js
@@ -0,0 +1,13 @@
+DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
+DR_EXPLAIN.data_index = {
+
+	// index
+	DREX_NODE_KEYWORDS: [3,1,2],
+	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,0,0,0,0,0,1,3,3,3], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,0,0,0,0,0,0,0,0,1,3,3,3,3], //length:= drex.nodes_count,
+
+	DREX_KEYWORD_NAMES: ["<NEW KEYWORD>","setari","aplicatia de navigare preferata","meniu utilizator"],
+	DREX_KEYWORD_CHILD_START: [1,4,4,4],
+	DREX_KEYWORD_CHILD_END: [4,4,4,4]
+	
+};
